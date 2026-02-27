c0ddc0f708e1142c05a59f29dc0f7a6b08de46ea
tss217 <thiago.sdasilva2090@gmail.com>
2026-02-27 13:32:33 -0300

    config

--- init/schema.sql
+++ init/schema.sql
@@ -1,9 +1,7 @@
-CREATE DATABASE IF NOT EXIST clean_database;
+CREATE DATABASE clean_database;
 
-CREATE TABLE IF NOT EXISTS 'clean_database'.'users' (
-    id BIGINT NOT NULL AUTO_INCREMENT,
+CREATE TABLE IF NOT EXISTS users (
+    id BIGSERIAL PRIMARY KEY,
     first_name VARCHAR(255) NOT NULL,
-    age BIGINt NOT NULL,
-    PRIMARY KEY(id)
+    age BIGINT NOT NULL
 );
-
